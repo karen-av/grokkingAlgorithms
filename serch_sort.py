03e0ced8b40e3580896d623d7c5f41a3110c7c36
Karen <ka.spk27@gmail.com>
2023-01-28 13:46:24 +0300

    1

---
+++ serch_sort.py
@@ -0,0 +1,20 @@
+def find_smoller(arr):
+    smoller = arr[0]
+    index_smoller = 0
+    for i in range(len(arr)):
+        if arr[i] < smoller:
+            smoller = arr[i]
+            index_smoller = i
+    return index_smoller
+
+def sort_choose(arr):
+    sort_arr = []
+    for i in range(len(arr)):
+        sort_arr.append(arr.pop(find_smoller(arr)))
+    return sort_arr
+
+
+x = [2,4,1, 7, 4, 6]
+print(sort_choose(x))
+        
+    
