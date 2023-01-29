a7e49eed74f39eb7b527694e52bf4ced35843b26
Karen <ka.spk27@gmail.com>
2023-01-29 15:57:04 +0300

    Update serch_sort.py

--- serch_sort.py
+++ serch_sort.py
@@ -1,5 +1,6 @@
+# Поиск наименьшего числа в массиве перебором всего  массива          
 def find_smoller(arr):
-    smoller = arr[0]
+    smoller = arr[0] 
     index_smoller = 0
     for i in range(len(arr)):
         if arr[i] < smoller:
@@ -7,6 +8,8 @@ def find_smoller(arr):
             index_smoller = i
     return index_smoller
 
+
+# сортировака массива
 def sort_choose(arr):
     sort_arr = []
     for i in range(len(arr)):
